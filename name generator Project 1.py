98f1dc8ae4a1026c2d126880e09b5862f3232bdb
Mazen Ahmed <mazenahmed1932003@gmail.com>
2025-11-07 15:30:19 +0200

    Add files via upload

---
+++ name generator Project 1.py
@@ -0,0 +1,8 @@
+print("welcome, to the youtube channel name generator!")
+name = input("what is your name? \n")
+print("hello " + name)
+print("let's generate a youtube channel name for you!")
+subject = input("what is the subject of your channel? \n")
+channel_name = subject + " with " + name
+print("your youtube channel name could be " + "[ "+ channel_name + " ]")
+print("thank you for using the youtube channel name generator!")
